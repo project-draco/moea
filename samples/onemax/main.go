b74dacbc74b2f78a06aba41173d961a8ac5740f7
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-01-14 13:13:13 -0200

    Add profiling

--- samples/onemax/main.go
+++ samples/onemax/main.go
@@ -1,53 +1,71 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math/big"
 	"math/rand"
 	"os"
+	"runtime/pprof"
 	"time"
 
 	"project-draco.io/moea"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer func() {
+			pprof.StopCPUProfile()
+			f.Close()
+		}()
+	}
 	rand.Seed(time.Now().UTC().UnixNano())
-	objectiveFunc := func(individual moea.Individual) float64 {
-		arr := individual.Value(0).([]big.Word)
-		result := 0.0
-		n := 0
-		for _, x := range arr {
-			for ; n < individual.Len() && x != 0; x >>= 1 {
-				if x&1 != 0 {
+	for i := 0; i < 100; i++ {
+		objectiveFunc := func(individual moea.Individual) float64 {
+			arr := individual.Value(0).([]big.Word)
+			result := 0.0
+			n := 0
+			for _, x := range arr {
+				for ; n < individual.Len() && x != 0; x >>= 1 {
+					if x&1 != 0 {
+						result++
+					}
+					n++
+				}
+			}
+			return result
+		}
+		_ /*objectiveFunc2*/ = func(individual moea.Individual) float64 {
+			arr := individual.Value(0).([]bool)
+			result := 0.0
+			for _, x := range arr {
+				if x {
 					result++
 				}
-				n++
 			}
+			return result
 		}
-		return result
-	}
-	_ /*objectiveFunc2*/ = func(individual moea.Individual) float64 {
-		arr := individual.Value(0).([]bool)
-		result := 0.0
-		for _, x := range arr {
-			if x {
-				result++
-			}
+		config := &moea.Config{
+			Algorithm:            moea.NewSimpleAlgorithm(10),
+			Population:           moea.NewRandomBinaryPopulation(300, []int{100}),
+			ObjectiveFunc:        objectiveFunc,
+			MaxGenerations:       40,
+			CrossoverProbability: 0.5,
+			MutationProbability:  0.01,
 		}
-		return result
+		result, objective, err := moea.Run(config)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(result, objective)
 	}
-	config := &moea.Config{
-		Algorithm:            moea.NewSimpleAlgorithm(10),
-		Population:           moea.NewRandomBinaryPopulation(300, []int{100}),
-		ObjectiveFunc:        objectiveFunc,
-		MaxGenerations:       40,
-		CrossoverProbability: 0.5,
-		MutationProbability:  0.01,
-	}
-	//moea.NewRandomBooleanPopulation(300, []int{100}),
-	result, objective, err := moea.Run(config)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	fmt.Println(result, objective)
 }
